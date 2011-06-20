70abe506456f021a31b7ee810e73b5a485b47341
dkukral <dkukral@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-06-20 19:45:40 +0000

    added city logging
    git-svn-id: https://plugins.svn.wordpress.org/postal-logger/trunk@399355 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- postal-logger.php
+++ postal-logger.php
@@ -3,7 +3,7 @@
 Plugin Name: Postal Logger	
 Plugin URI: http://yourdomain.com/
 Description: Used to log postal codes of users coming to your blog.
-Version: 0.0
+Version: 0.1
 Author: Don Kukral
 Author URI: http://yourdomain.com
 License: GPL
@@ -11,16 +11,39 @@ License: GPL
 
 
 function postal_logger_menu() {
-	$log = unserialize(get_option('postal_logger', serialize(array())));
+    if ($_GET['view'] == 'city') {
+        $log = get_option('postal_logger_city', serialize(array()));
+    } else {
+        $log = get_option('postal_logger', serialize(array()));
+    }
 	asort($log);
 ?>
 	<div class="wrap">
 	<h2>Postal Logger</h2>
+	<ul id="postal_logger_menu">
+<?php
+    if ($_GET['view'] == 'city') {
+        echo '<li><a href="?page=Postal-Logger">Postal Code</a></li>';
+        echo '<li class="current">City</li>';
+    } else {
+        echo '<li class="current">Postal Code</li>';
+        echo '<li><a href="?page=Postal-Logger&view=city">City</a></li>';
+    }
+?>
+	</ul>
 	<p>
 	<table class="widefat">
 	<thead>
 	<tr>
-	<th scope="col">Postal Code</th>
+	<th scope="col">
+<?php
+    if ($_GET['view'] == 'city') {
+        echo 'City';
+    } else {
+        echo 'Postal Code';
+    }
+?>	   
+	</th>
 	<th scope="col">Count</th>
 	</tr>
 	</thead>
@@ -34,6 +57,42 @@ function postal_logger_menu() {
 	</table>
 	</p>
 	</div>
+	
+	<style type="text/css">
+    #postal_logger_menu {
+    	line-height: 1.4em;
+    	margin: 5px 0 0 0;
+    	padding: 0;
+    	border-bottom: 1px solid #aaaaaa;
+    }
+    #postal_logger_menu li { 
+    	border: 1px solid #aaaaaa;
+    	border-bottom: none;
+    	line-height: 1.4em;
+    	display: inline-block;
+    	margin: 0 5px 0 0;
+    	padding: 0;
+    	list-style-type: none;
+    	list-style-image: none;
+    	list-style-position: outside;
+    }
+    #postal_logger_menu li.current span { 
+    	background-color: #ffffff;
+    	font-weight: bold;
+    	padding: 0 5px 3px 5px;
+    }
+    #postal_logger_menu li a,
+    #postal_logger_menu li a:visited {
+    	padding: 0 5px;
+    	text-decoration: none;
+    }
+    #postal_logger_menu li a:hover {
+    	background-color: #eaf2fa;
+    }
+    #postal_logger_menu + .wrap {
+    	margin-top: 0;
+    }
+    </style>
 <?php
 }
 
@@ -48,8 +107,9 @@ add_action('init', 'capture_postal_code');
 function capture_postal_code() {
 	if (!session_id())
 		session_start();
+		
 	$location = geoip_record_by_name($_SERVER['REMOTE_ADDR']);
-
+    
 	$log = unserialize(get_option('postal_logger', serialize(array())));
 	if (!$_COOKIE['postal_code']) {
 		if ($location) {
@@ -70,6 +130,29 @@ function capture_postal_code() {
 		}
 
 		update_option('postal_logger', serialize($log));
+	}
+	
+	$log_city = unserialize(get_option('postal_logger_city', serialize(array())));
+
+	if (!$_COOKIE['city']) {
+		if ($location) {
+			$city = $location['city'];
+			if ($city == '') { $city = 'unknown'; }
+		} else {
+			$city = 'unknown';
+		}
+		$city = "".$city;
+		setcookie("city", $city, time()+3600, "/", 
+			str_replace('http://','',get_bloginfo('url')));
+		if ($city != 'unknown') {
+			if (isset($log_city[$city])) {
+				$log_city[$city] = intval($log_city[$city]) + 1;
+			} else {
+				$log_city[$city] = 1;
+			}
+		}
+
+		update_option('postal_logger_city', serialize($log_city));
 	}
 
 }
